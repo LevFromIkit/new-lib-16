ddd9af640c1b5751d956cf56b32762158e8df7e0
LevP <119807443+LevFromIkit@users.noreply.github.com>
2025-11-11 03:32:00 +0700

    Add files via upload

---
+++ fesm2022/testi-libric-my.mjs
@@ -0,0 +1,39 @@
+import * as i0 from '@angular/core';
+import { Component } from '@angular/core';
+
+class TestiLib {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: TestiLib, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: TestiLib, isStandalone: true, selector: "libiiiii-testi-lib", ngImport: i0, template: `
+    <p>
+      testi-lib works!ewqewqewerewr
+    </p>
+  `, isInline: true });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: TestiLib, decorators: [{
+            type: Component,
+            args: [{ selector: 'libiiiii-testi-lib', imports: [], template: `
+    <p>
+      testi-lib works!ewqewqewerewr
+    </p>
+  ` }]
+        }] });
+
+class Ppppppp {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: Ppppppp, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: Ppppppp, isStandalone: true, selector: "libiiiii-ppppppp", ngImport: i0, template: "<p>LLLLLL llll lll lll  fsdfsfsdfsd</p>\r\n<span>dsfedgrtyhyth</span>\r\n<title>dsdasfddsfsdf</title>", styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: Ppppppp, decorators: [{
+            type: Component,
+            args: [{ selector: 'libiiiii-ppppppp', imports: [], standalone: true, template: "<p>LLLLLL llll lll lll  fsdfsfsdfsd</p>\r\n<span>dsfedgrtyhyth</span>\r\n<title>dsdasfddsfsdf</title>" }]
+        }] });
+
+/*
+ * Public API Surface of testi-lib
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { Ppppppp, TestiLib };
+//# sourceMappingURL=testi-libric-my.mjs.map
